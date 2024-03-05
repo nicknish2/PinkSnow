472dc1e257a9a204ea5ffde8ceb3439c55ab5710
Milan <milankloewer@gmx.de>
2024-03-05 18:10:39 -0500

    data loader for specific stations only

--- data/manual_data_loader.jl
+++ data/manual_data_loader.jl
@@ -1,6 +1,19 @@
 using HTTP, Dates, Printf
 
 path = "https://www.nohrsc.noaa.gov/nsa/discussions_text/Northeast/snowdepth/"
+stations = ["HERMIT LAKE", 
+            "MOUNT WASHINGTON",
+            "HARVARD CABIN",
+            "CARTER NOTCH",
+            "ZEALAND FALLS HUT",
+            "LONESOME LAKE HUT",
+            "GROTON",
+            "CRAWFORD NOTCH",
+            "PINKHAM NOTCH",
+            "JACKSON",
+            "NORTH CONWAY",
+            "MOUNT MANSFIELD",
+            ]
 
 year = DateTime(2024)
 season_start = Day(-61)
@@ -21,11 +34,31 @@ for date in season
 
     try
         r = HTTP.request("GET", fullpath)
+        success = true
+    catch
+        success = false
+        @info "HTTP request failed: $date was not available."
+    end
+
+    if success
         txtfile = open(output_path,"w")
-        println(txtfile,String(r.body))
+        body = split(String(r.body),"\n")
+
+        # print header
+        for line in body[1:2]
+            println(txtfile,line)
+        end
+
+        # print stations
+        for line in body[3:end]
+            for station in stations
+                if occursin(station,line)
+                    println(txtfile,line)
+                end
+            end
+        end
+
         close(txtfile)
         @info "$date written to file."
-    catch
-        @info "HTTP request failed: $date was not available."
     end
 end
